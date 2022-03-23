a507f5f1a1d7e6c8cb6dd15365b027fdd1718f2b
Nate Crawford <natec@jntcompany.com>
2022-03-23 15:54:26 -0500

    add JS from original repo

---
+++ js/index.js
@@ -0,0 +1,37 @@
+require('bootstrap/dist/js/bootstrap.js');
+
+/* sidebar toggle */
+Array.from(document.getElementsByClassName('toggle-sidebar-btn')).forEach((e) =>
+    e.addEventListener('click', function () {
+        document.body.classList.toggle('toggle-sidebar');
+    })
+);
+
+/* header scrolled */
+let selectHeader = document.getElementById('header');
+if (selectHeader) {
+    const headerScrolled = () => {
+        if (window.scrollY > 100) {
+            selectHeader.classList.add('header-scrolled');
+        } else {
+            selectHeader.classList.remove('header-scrolled');
+        }
+    };
+    window.addEventListener('load', headerScrolled);
+    onscroll(document, headerScrolled);
+}
+
+/* back to top */
+let backtotop = document.getElementsByClassName('back-to-top');
+if (backtotop) {
+    backtotop = backtotop[0];
+    const toggleBacktotop = () => {
+        if (window.scrollY > 100) {
+            backtotop.classList.add('active');
+        } else {
+            backtotop.classList.remove('active');
+        }
+    };
+    window.addEventListener('load', toggleBacktotop);
+    document.addEventListener('scroll', toggleBacktotop);
+}
